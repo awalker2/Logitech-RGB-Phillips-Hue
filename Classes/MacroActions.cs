8e8ade0ac15ea3b49a2a5cbae6afca044971e313
Alex Walker <awalker2@stevens.edu>
2019-02-02 20:06:37 -0500

    Initial commit, got Logitech SDKs imported, basic color changing on keyaboard only

---
+++ Classes/MacroActions.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LedCSharp;
+
+namespace LogitechRGBTest.Classes
+{
+    class MacroActions
+    {
+        public MacroActions()
+        {
+            // Initialize the LED SDK
+            bool LedInitialized = LogitechLEDGSDK.LogiLedInitWithName("Test");
+            if (!LedInitialized)
+            {
+                Console.WriteLine("LogitechGSDK.LogiLedInit() failed.");
+            }
+            Console.WriteLine("LED SDK Initialized");
+            LogitechLEDGSDK.LogiLedSetTargetDevice(LogitechLEDGSDK.LOGI_DEVICETYPE_ALL);
+
+            // Initialize the Macro SDK with callback implementation
+            LogitechMacroGSDK.logiGkeyCB cbInstance = new LogitechMacroGSDK.logiGkeyCB((this.gkeySDKCallback));
+            LogitechMacroGSDK.LogiGkeyInitWithoutContext(cbInstance);
+        }
+
+        public void gkeySDKCallback(LogitechMacroGSDK.GkeyCode gKeyCode, String gKeyOrButtonString, IntPtr context)
+        {
+            if (gKeyCode.keyDown == 0)
+            {
+                if (gKeyCode.mouse == 1)
+                {
+                    // Handle gkey released mouse
+                }
+                else
+                {
+                    // Handle gkey released on keyboard/headset
+                }
+            }
+            else
+            {
+                if (gKeyCode.mouse == 1)
+                {
+                    // Handle gkey pressed on mouse
+                }
+                else
+                {
+                    // Handle gkey pressed on keyboard/headset
+                    Console.WriteLine("Keyboard pressed: " + gKeyOrButtonString);
+                }
+            }
+        }
+
+        ~ MacroActions()
+        {
+            // Shutdown
+            LogitechMacroGSDK.LogiGkeyShutdown();
+            LogitechLEDGSDK.LogiLedShutdown();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LedCSharp;
+using LogitechRGBTest.Classes;
+
+namespace Logi_SetTargetZone_Sample_CS
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            MacroActions actions = new MacroActions();
+
+            // Set all devices to Black
+            LogitechLEDGSDK.LogiLedSetLighting(0, 0, 0);
+
+            // Set some keys on keyboard
+            LogitechLEDGSDK.LogiLedSetLightingForKeyWithKeyName(keyboardNames.L, 0, 100, 100);
+            LogitechLEDGSDK.LogiLedSetLightingForKeyWithKeyName(keyboardNames.O, 0, 100, 100);
+            LogitechLEDGSDK.LogiLedSetLightingForKeyWithKeyName(keyboardNames.G, 0, 100, 100);
+            LogitechLEDGSDK.LogiLedSetLightingForKeyWithKeyName(keyboardNames.I, 0, 100, 100);
+
+
+            // Set G633/G933 headset logos to White, backsides to Purple
+            LogitechLEDGSDK.LogiLedSetLightingForTargetZone(DeviceType.Headset, 0, 100, 100, 100);
+
+            LogitechLEDGSDK.LogiLedSetLightingForTargetZone(DeviceType.Headset, 1, 100, 0, 100);
+
+            Console.WriteLine("Press \"ENTER\" to continue...");
+            Console.ReadLine();
+
+            // Set some keys on keyboard
+            LogitechLEDGSDK.LogiLedSetLightingForKeyWithKeyName(keyboardNames.L, 100, 100, 100);
+            LogitechLEDGSDK.LogiLedSetLightingForKeyWithKeyName(keyboardNames.O, 100, 100, 100);
+            LogitechLEDGSDK.LogiLedSetLightingForKeyWithKeyName(keyboardNames.G, 100, 100, 100);
+            LogitechLEDGSDK.LogiLedSetLightingForKeyWithKeyName(keyboardNames.I, 100, 100, 100);
+
+
+            Console.WriteLine("Press \"ENTER\" to continue...");
+            Console.ReadLine();
+
+
+            Console.WriteLine("LED SDK Shutting down");
+        }
+    }
+}
